86379fae38ea22d43073b287a206312e1cfce8cc
u0a377 <83986313+u0a377@users.noreply.github.com>
2021-08-17 22:32:36 +0100

    Install.sh

--- install.sh
+++ install.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 #Author: github.com/thelinuxchoice
-#Instagram: @thelinuxchoice
+#Instagram: @thelinuxchoicii
 trap 'echo exiting cleanly...; exit 1;' SIGINT SIGTSTP
 
-checkroot() {
-
-if [[ "$(id -u)" -ne 0 ]]; then
-   printf "\e[1;77mPlease, run this program as root!\n\e[0m"
-   exit 1
-fi
-
-}
-
-checkroot
 
 (trap '' SIGINT SIGTSTP && command -v tor > /dev/null 2>&1 || { printf >&2  "\e[1;92mInstalling TOR, please wait...\n\e[0m"; apt-get update > /dev/null && apt-get -y install tor > /dev/null || printf "\e[1;91mTor Not installed.\n\e[0m"; }) & wait $!
 
